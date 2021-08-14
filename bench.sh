aa7f22cd2200fc91d183ceee2efdb1be289034b8
Shuichi Hirahara <hirahara.shuichi@gmail.com>
2021-08-14 16:57:45 +0900

    addded bench

---
+++ bench.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eu
+
+HOSTS=$(echo isucon@app{1,2,3})
+
+if [ "${1:-}" = "-h" ]
+then
+	echo "Usage: $0 [git branch]" 2>&1
+	exit 1
+fi
+
+cd $(dirname $(readlink -f $0))
+source ./tools/util.sh
+
+if [ $# -ge 1 ]
+then
+	BRANCH=$1
+	info "git pull && git checkout $BRANCH"
+	git pull && git checkout $BRANCH
+fi
+
+info "Execute init.sh"
+xpanes -c "ssh {} -t 'init.sh'" $HOSTS
+
+info "Execute benchmark"
+# curl ...
+ssh isucon@app1 -t 'cd ~/isuumo/bench/ && ./bench'
+
