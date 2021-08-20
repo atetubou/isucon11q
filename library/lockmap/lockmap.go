0434a0fddf69974c4aefb71c9bfa9e420858df1e
Shuichi Hirahara <hirahara.shuichi@gmail.com>
2021-08-20 13:34:54 +0900

    document for lockmap

--- library/lockmap/lockmap.go
+++ library/lockmap/lockmap.go
@@ -1,3 +1,35 @@
+/*
+Lockmap extends sync.Map by adding a lock.
+ELement is the type of objects that can be stored in Lockmap.
+Element has a function Load(), which typically returns a copied version of itself.
+
+Typically, one uses https://github.com/globusdigital/deep-copy to implement the Load() function.
+deep-copy --type LockStruct PACKAGENAME
+
+Example:
+	type TestStruct struct {
+		name string
+	}
+	func (v TestStruct) Load() interface{} {
+		w := v
+		return w
+	}
+	func main() {
+		l := LockMap{}
+		l.Store(10, &TestStruct{
+			name: "test",
+		})
+		l.Update(10, func(value Element) {
+			val := value.(*TestStruct)
+			val.name = "hoge"
+		})
+		res, ok := l.Load(10)
+		if !ok {
+			log.Fatal("Not found")
+		}
+		log.Println(res.(TestStruct).name)
+	}
+*/
 package lockmap
 
 import (
@@ -19,7 +51,7 @@ type Element interface {
 	Load() interface{}
 }
 
-func (lm *LockMap) Update(key interface{}, f func(value interface{})) bool {
+func (lm *LockMap) Update(key interface{}, f func(value Element)) bool {
 	u, ok := lm.Map.Load(key)
 	if !ok {
 		return false
