0cde958bc4a9491f943859ea4297a9d7d99eb4da
isucon <isucon@example.com>
2021-08-21 04:30:46 +0000

    bench

---
+++ post.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+
+HOSTS=$(echo app{1,2,3})
+cd $(dirname $(readlink -f $0))
+source ./tools/util.sh
+
+tmux set-window-option synchronize-panes 0
+xpanes -x -s -c "rsync -Cva {}:~/isucon11q/tools/log ~/ && exit"  $HOSTS
+tmux set-window-option synchronize-panes 1
+read -p 'Press enter to continue> '
+make -j 4 -C ~/
+
