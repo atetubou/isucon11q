dfb24192d237b2daf6cbf3a37d4220900d930db5
Shuichi Hirahara <hirahara.shuichi@gmail.com>
2021-08-21 10:53:02 +0900

    initialize all

---
+++ go/rpcgroup/server.go
@@ -0,0 +1,33 @@
+package rpcgroup
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
+
+// Hostname returns host name or "" if there is an error
+func Hostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = ""
+	}
+	return hostname
+}
+
+func init() {
+	dummy := new(Dummy)
+	rpc.Register(dummy)
+	rpc.HandleHTTP()
+}
+
+func Listen(listenPort int) {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", listenPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %d\nError: %v", listenPort, err)
+	}
+	go http.Serve(l, nil)
+}
